66f3325a88423920847126a7c7df2f4545f99b06
Andrea <andrea.company@inetum.com>
2025-04-03 12:30:48 +0200

    Initial commit

---
+++ src/main/java/es/inditexprueba/hexagonalarchitecturepriceapi/HexagonalArchitecturePriceApiApplication.java
@@ -0,0 +1,13 @@
+package es.inditexprueba.hexagonalarchitecturepriceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitecturePriceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitecturePriceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/inditexprueba/hexagonalarchitecturepriceapi/HexagonalArchitecturePriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.inditexprueba.hexagonalarchitecturepriceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitecturePriceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
